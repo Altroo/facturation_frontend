429e71423339bb1ca943f889c0c1eaa3d2fcb70d
Youness <service-it@casadilusso.ma>
2025-11-04 11:11:27 +0100

    refactor: add .eslintignore file and update SASS imports for color usage consistency

--- src/app/ThemeRegistry.tsx
+++ src/app/ThemeRegistry.tsx
@@ -1,16 +1,11 @@
 'use client';
 
 import * as React from 'react';
-import { ThemeProvider, CssBaseline } from '@mui/material';
+import { ThemeProvider } from '@mui/material';
 import { getDefaultTheme } from '@/utils/themes';
 
 const ThemeRegistry = ({ children }: { children: React.ReactNode }) => {
-	return (
-		<ThemeProvider theme={getDefaultTheme()}>
-			<CssBaseline />
-			{children}
-		</ThemeProvider>
-	);
+	return <ThemeProvider theme={getDefaultTheme()}>{children}</ThemeProvider>;
 };
 
 export default ThemeRegistry;
