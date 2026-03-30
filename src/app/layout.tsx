3ff1e2522f88d44d127c5b28b87613a700c6e36c
Altroo <scorpion.yooy@gmail.com>
2026-03-30 09:00:58 +0100

    feat: refactor RootLayout to fetch session client-side and simplify rendering logic

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,7 +6,6 @@ import SessionProvider from '@/providers/sessionProvider';
 import StoreProvider from '@/providers/storeProvider';
 import type { AppProps } from 'next/app';
 import { InitContextProvider } from '@/contexts/InitContext';
-import { auth } from '@/auth';
 import type { Viewport } from 'next';
 import ThemeProvider from '@/providers/themeProvider';
 import { InitEffects } from '@/contexts/initEffects';
@@ -68,15 +67,14 @@ interface EntryPointProps extends AppProps {
 	children: React.ReactNode;
 }
 
-const RootLayout: React.FC<EntryPointProps> = async (props) => {
-	const session = await auth();
+const RootLayout: React.FC<EntryPointProps> = (props) => {
 	return (
 		<html lang="fr" data-scroll-behavior="smooth">
 			<body>
 				<a href="#main-content" className="skip-to-content">
 					Aller au contenu principal
 				</a>
-				<SessionProvider session={session}>
+				<SessionProvider>
 					<StoreProvider>
 						<InitContextProvider>
 							<InitEffects />
