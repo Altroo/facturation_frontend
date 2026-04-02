92c0dd7c5ef41ffd20f6a54d8c7ca7b8a9264411
Altroo <scorpion.yooy@gmail.com>
2026-04-02 14:49:13 +0100

    feat: enable client-side rendering in hooks by adding 'use client' directive

--- src/utils/hooks.ts
+++ src/utils/hooks.ts
@@ -1,3 +1,4 @@
+'use client';
 import { useContext, useSyncExternalStore } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import type { TypedUseSelectorHook } from 'react-redux';
