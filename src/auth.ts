b5d510bf7ab922b3973a7cf81bf1d0a45276ed51
Altroo <scorpion.yooy@gmail.com>
2026-05-13 16:04:49 +0100

    fix(auth): use absolute login route

--- src/auth.ts
+++ src/auth.ts
@@ -201,8 +201,8 @@ export const { handlers, auth } = NextAuth({
 	cookies: getAuthCookies('ebh-facturation'),
 
 	pages: {
-		signIn: 'login',
-		error: 'login',
+		signIn: '/login',
+		error: '/login',
 	},
 
 	callbacks: {
